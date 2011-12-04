c1445e9b3cd163e3027f7aead26ca8715c83f63f
Paul Mikols <PJMikols@mac.com>
2011-12-03 18:41:03 -0600

    Update the Credentials
    allows multiple Wifi connections.

--- Credentials.h
+++ Credentials.h
@@ -2,7 +2,17 @@
 #define __CREDENTIALS_H__
 
 // Wifi parameters
-char passphrase[] = "PASSPHRASE;
-char ssid[] = "SSID";
+char passphrase1[] = "PASSPHRASE;
+char ssid1[] = "SSID";
+
+char passphrase2[] = "PASSPHRASE;
+char ssid2[] = "SSID";
+
+char passphrase3[] = "PASSPHRASE;
+char ssid3[] = "SSID";
+
+char passphrase4[] = "PASSPHRASE;
+char ssid4[] = "SSID";
+
 
 #endif
