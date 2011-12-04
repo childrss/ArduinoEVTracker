26ef33b6a0a2b0998c04c1f3daca341a9cf6e39f
Paul Mikols <PJMikols@mac.com>
2011-12-03 19:31:07 -0600

    GSM responses
    Clarify what the GSM responses do.

---
+++ GSM.h
@@ -0,0 +1,19 @@
+
+
+/*
+*Format: 
+
+*<Secret code(4)> : Action # :  Action event
+* ####:0    Reply Back to Number with Stats
+* ####:1    Reply to Default SMS with Stats.
+* ####:2    Default EMail Address
+* ####:3    Activate Sleep Mode
+* ####:4    Deactivate Sleep Mode
+* ####:5    Power Down GPRS    >>>>>>>>> don't need
+* ####:6    Enable Motion Detection
+* ####:7    Disable Motion Dection
+* ####:8    < Availible>
+* ####:9    Battery Report  >>>>>>>>>>> Don't need
+
+
+*/
